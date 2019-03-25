c32a3f99c6cc05b364b33fbf4d1c20bd66649139
Xuehai-Chen <13162576651@163.com>
2019-03-25 15:16:07 +0000

    add batch process sys feature

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -1,21 +1,19 @@
 #include "common.h"
 #include "syscall.h"
+#include "proc.h"
 
 int fs_open(const char*, int, int);
 ssize_t fs_read(int, void*, size_t);
 size_t fs_write(int fd, const void* buf, size_t len);
 off_t fs_lseek(int, off_t, int);
 int fs_close(int);
-
+void naive_uload(PCB*, const char*);
 
 _Context* do_syscall(_Context *c) {
 	uintptr_t a[4];
 	a[0] = c->GPR1;
 
 	switch (a[0]) {
-		case SYS_exit:
-			_halt(c->GPR2);
-			break;
 		case SYS_yield:
 			_yield();
 			c->GPR1 = 0;
@@ -37,6 +35,11 @@ _Context* do_syscall(_Context *c) {
 			break;
 		case SYS_brk:
 			c->GPR1 = 0;
+			break;
+		case SYS_exit:
+			c->GPR2 = (intptr_t)&"/bin/init";
+		case SYS_execve:
+			naive_uload(NULL, (char*)c->GPR2);
 			break;
 		default: panic("Unhandled syscall ID = %d", a[0]);
 	}
